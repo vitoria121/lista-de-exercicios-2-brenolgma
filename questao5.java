0545be67f36174368f91165c21d8500c1a4696fe
profbreno <brenod123@gmail.com>
2021-08-17 15:00:05 -0300

    Questão 5 completa

--- questao5.java
+++ questao5.java
@@ -1,6 +1,23 @@
 public class questao5 {
 
+    /*
+     * Construa a tabela de multiplicação de números de 1 a 10 (ex.: 1 x 1 = 1, 1 x
+     * 2 = 2, etc.).
+     */
     public static void main(String[] args) {
+
+        int i, j;
+
+        for (i = 1; i <= 10; i++) {
+
+            System.out.println("Tab. de " + i);
+
+            for (j = 1; j <= 10; j++) {
+
+                System.out.println(i + " x " + j + " = " + (i * j));
+
+            }
+        }
 
     }
 
