35842395dd422c8bff90fa56c380ff21a02fa1eb
brenolgma <88721806+brenolgma@users.noreply.github.com>
2021-08-19 10:25:40 -0300

    Renomeado a questao1 para Questão1

--- Questao1.java
+++ Questao1.java
@@ -5,7 +5,7 @@ Determine as raízes de uma equação de 2º grau: ax2 + bx + c = 0
 (recordar que o discriminante Δ = b2 – 4ac, e que a raiz r = (–b ± √Δ)/2a).
 */
 
-public class questao1 {
+public class Questao1 {
 
     public static void main(String[] args) {
 
