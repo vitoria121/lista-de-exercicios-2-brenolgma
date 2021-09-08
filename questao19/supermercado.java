3456bd3eb53ba12aa248ca4bbe11dde91dfa321b
profbreno <brenod123@gmail.com>
2021-09-08 16:56:45 -0300

    Update supermercado.java

--- questao19/supermercado.java
+++ questao19/supermercado.java
@@ -7,14 +7,16 @@ quantidade em estoque. Um pedido de um cliente é composto de itens, onde cada
 item especifica o produto que o cliente deseja e a respectiva quantidade. Esse
 pedido pode ser pago em dinheiro, cheque ou cartão.”
 
-Atividade para 08/09/2021
+PROVA 3 - UNIDADE 1
+Entrega até 14/09/2021 às 23:59
 
 - Usuário que deve Adicionar itens ao pedido (Apresente a lista de produtos e
 quantidade disponível)
 - Editar o pedido (Apresente a lista de produtos e quantidade disponível)
 - Usuário que deve remover itens do pedido
 - Usuário que deve escolher a forma de pagamento do pedido após o pedido ser
-finalizado
+finalizado (Dinheiro ou Cartão)
+- Se dinheiro, o usuário deve informar o valor do pagamento e o troco deve ser apresentado
 */
 
 public class supermercado {
